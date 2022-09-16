432a0dd7a399e815e68143b2e38169fd41c24aba
hhc <hhc@163.com.cn>
2022-09-16 10:23:23 +0800

    first commit

---
+++ src/main/java/com/hhc/test/GitTest.java
@@ -0,0 +1,11 @@
+package com.hhc.test;
+
+/**
+ * @author hhc
+ * @create 2022-09-16 10:16
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("first commit");
+    }
+}
